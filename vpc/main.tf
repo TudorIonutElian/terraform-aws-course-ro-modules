18bc040a05e009d0c9b4ee718e9c2919c1779d46
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-09-10 14:54:22 +0300

    Adding variables to modules

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "primul_vpc" {
   cidr_block = "10.0.0.0/16"
-  count = 1
+  count = var.numar_de_instance_vpc
 
   tags = {
-    "my_first_tag" = "my_first_tag_value"
-    "my_second_tag" = "my_second_tag_value"
+    "my_first_tag" = "my_first_tag_value_module"
+    "my_second_tag" = "my_second_tag_value_module"
   }
 }
---
+++ vpc/variable.tf
@@ -0,0 +1,5 @@
+variable "numar_de_instance_vpc" {
+  type = number
+  description = "Numarul de VPC-uri"
+  default = 2  
+}
