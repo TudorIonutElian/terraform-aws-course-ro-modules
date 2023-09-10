12b04d0adbb99e21b47acfafbcc6bfbe6d3fe23e
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2023-09-10 22:05:22 +0300

    Fix aws instance type

--- main/outputs.tf
+++ main/outputs.tf
@@ -1,4 +1,4 @@
-output "module_ec2_ec2_ami_id" {
+output "module_ec2_instance_type" {
   description = "Vom prelua instance type de la child module"
   value = module.module_ec2.ec2_module_instance_type
 }
